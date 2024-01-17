38a92a91f0e6e34617f616529e7386a3b2b06f87
PHT1232 <57097105+PHT1232@users.noreply.github.com>
2024-01-17 19:01:45 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sideBar/sidebar/sidebar.component';
-import { CreateTagComponent } from './recipt/tag/create-tag/create-tag.component';
-import { TagComponent } from './recipt/tag/tag.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 //Material UI
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -22,9 +21,6 @@ import { MatButtonModule } from '@angular/material/button';
 //PrimeNg UI
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
-import { ReciptComponent } from './recipt/recipt.component';
-import { ReceiptCreateComponent } from './recipt/receipt-create/receipt-create.component';
-import { PanelModule } from 'primeng/panel';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { PaginatorModule } from 'primeng/paginator';
 import { ButtonModule } from 'primeng/button';
@@ -35,6 +31,9 @@ import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
 import { TableModule } from 'primeng/table';
 import { SpeedDialModule } from 'primeng/speeddial';
 import { DialogModule } from 'primeng/dialog';
+import { TabViewModule } from 'primeng/tabview';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
 
 //Ngx
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
@@ -46,15 +45,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TeamsComponent } from './teams/teams.component';
 import { CreateTeamsComponent } from './teams/create-teams/create-teams.component'
 
+const config: SocketIoConfig = { url: 'http://localhost:7279', options: {} };
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ReciptComponent,
-    ReceiptCreateComponent,
     ModalFooterComponent,
-    CreateTagComponent,
-    TagComponent,
     TeamsComponent,
     CreateTeamsComponent,
   ],
@@ -70,7 +66,6 @@ import { CreateTeamsComponent } from './teams/create-teams/create-teams.componen
     MatFormFieldModule,
     MatToolbarModule,
     MatButtonModule,
-    PanelModule,
     FormsModule,
     ScrollPanelModule,
     PaginatorModule,
@@ -86,7 +81,11 @@ import { CreateTeamsComponent } from './teams/create-teams/create-teams.componen
     TableModule,
     DialogModule,
     HttpClientModule,
-    SpeedDialModule
+    SpeedDialModule,
+    TabViewModule,
+    InputMaskModule,
+    InputTextModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [
     DialogService
